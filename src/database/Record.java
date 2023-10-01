775d5dba91a36768a45a17cfd4db5ed56204471a
karthikstar <64301417+karthikstar@users.noreply.github.com>
2023-10-01 20:28:05 +0800

    Add record class

--- src/database/Record.java
+++ src/database/Record.java
@@ -1,4 +1,14 @@
 package database;
 
 public class Record {
+
+    private String GAME_DATE_EST;
+    private int TEAM_ID_home;
+    private int PTS_HOME;
+    private float FG_PCT_home;
+    private float FT_PCT_home;
+    private float FG3_PCT_home;
+    private int AST_home;
+    private int REB_home;
+    private int HOME_TEAM_WINS;
 }
