2f037b31790d9e5e92c87636fb5dc938daff219d
karthikstar <64301417+karthikstar@users.noreply.github.com>
2023-10-07 16:15:39 +0800

    Add Disk and Cache changes

--- src/database/Disk.java
+++ src/database/Disk.java
@@ -1,20 +1,71 @@
 package database;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Disk {
+    private Cache cache;
+
+    private Block[] blocks;
+
+    private Set<Integer> availBlocks;
+    private Set<Integer> fullBlocks;
+
+
+    private static int noOfBlockAccess = 0;
+    private int noOfBlockAccessReduced = 0;
+
+
     private int blockSize;
 
     private int diskCapacity;
 
-    private static int totalNoOfRecords = 0; // no. of records stored in disk
+    private int totalNoOfRecords = 0; // no. of records stored in disk
 
     public Disk(int blockSize, int diskCapacity) {
         this.blockSize = blockSize;
         this.diskCapacity = diskCapacity;
+        this.blocks = new Block[diskCapacity / blockSize];
+        this.availBlocks = new HashSet<>();
+        this.fullBlocks = new HashSet<>();
+
+        // init blocks
+        for (int j = 0; j < blocks.length; j++) {
+            blocks[j] = new Block(blockSize);
+            availBlocks.add(j);
+        }
+
+        // TODO: check this code agn
+        int cacheSize = (int) (256.0 / 500000.0 * diskCapacity / blockSize);
+        this.cache = new Cache(cacheSize);
+
     }
 
-//    public static Address writeRecToDisk(Record record) {
+//    public Address writeRecToDisk(Record record) {
 //        totalNoOfRecords++;
-
-        // TODO: finish this implementation to get block id of first avail block, and inserting record into block and getting a address.
+//        int blockAddress = getFirstAvailableBlockId();
+//
+//
 //    }
+//
+//    private int getFirstAvailableBlockId() {
+//        if(availBlocks.isEmpty()) {
+//
+//        }
+//    }
+
+    public int getTotalNoOfRecords() {
+        return totalNoOfRecords;
+    }
+
+    public int getNoOfBlockAccess() {
+        return noOfBlockAccess;
+    }
+
+    public int getNoOfBlockAccessReduced() {
+        return noOfBlockAccessReduced;
+    }
+
+
+
 }
